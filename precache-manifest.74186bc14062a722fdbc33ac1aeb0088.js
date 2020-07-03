f2cd37aa4d7e1c15ac813e61abefd50354369ae2
Matan Shaviro <matanshaviro@matans-mbp-2.mynet>
2020-07-03 15:15:26 +0300

    Updates

---
+++ precache-manifest.74186bc14062a722fdbc33ac1aeb0088.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbb3a0290dfb3702346c96ecb92e3dd1",
+    "url": "/matan3sh.github.io/videotube/index.html"
+  },
+  {
+    "revision": "09e270d547935ae09259",
+    "url": "/matan3sh.github.io/videotube/static/css/main.417ed544.chunk.css"
+  },
+  {
+    "revision": "8b9f38a67c79aa94ba98",
+    "url": "/matan3sh.github.io/videotube/static/js/2.c66ceb12.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/matan3sh.github.io/videotube/static/js/2.c66ceb12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09e270d547935ae09259",
+    "url": "/matan3sh.github.io/videotube/static/js/main.9d7e11f8.chunk.js"
+  },
+  {
+    "revision": "5f9066de17cbba14b082",
+    "url": "/matan3sh.github.io/videotube/static/js/runtime-main.01023df7.js"
+  }
+]);
